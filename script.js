1308c6c5d63f99aa5e15b9891bd3dddbb45d7217
Anisha-khadka <anishakhadka434@gmail.com>
2024-05-20 18:33:19 +0545

    second commit

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 function storemsg() {
-    console.log("Function called");
-  
-    var firstname = document.getElementById("firstname").value;
-    let lastname = document.getElementById("lastname").value;
-    var emailaddress = document.getElementById("emailaddress").value;
-    var number = document.getElementById("number").value;
-    var textarea = document.getElementById("inputText").value;
+  console.log("Function called");
 
-     if((firstname==="") || (lastname==="") || (emailaddress==="") || (number==="") || (textarea==="")){
-        alert("All fields are required");
-     }
-  else{
-     var contact={
-        firstname,
-     lastname,
+  var firstname = document.getElementById("firstname").value;
+  let lastname = document.getElementById("lastname").value;
+  var emailaddress = document.getElementById("emailaddress").value;
+  var number = document.getElementById("number").value;
+  var textarea = document.getElementById("inputText").value;
+
+  if (
+    firstname === "" ||
+    lastname === "" ||
+    emailaddress === "" ||
+    number === "" ||
+    textarea === ""
+  ) {
+    alert("All fields are required");
+  } else {
+    var contact = {
+      firstname,
+      lastname,
       emailaddress,
-       number,
-        textarea
+      number,
+      textarea,
+    };
 
-        
-
-     }
-
-     localStorage.setItem("contact",JSON.stringify(contact));
-     alert("success")
-    }
-    }
-
-
+    localStorage.setItem("contact", JSON.stringify(contact));
+  }
+}
